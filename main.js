a22e2ee703ca235991e2c3bc273b48b96efdcc3c
TECH-Net-15 <120729062+TECH-Net-15@users.noreply.github.com>
2022-12-26 15:41:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+       window.onload = function(){
+       let contenedor = document.getElementById('contenedor_carga');
+         
+             contenedor.style.visibility = 'hidden';
+             contenedor.style.opacity = '0';
+             }
+           
+           
+             ;
+             
+    
+               
+
+              
+             
+   
+         
